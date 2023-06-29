840cb9ce0fdf0fc22022420f33cbef753c7cf86f
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-06-29 15:51:05 -0300

    Estrutura server do projeto

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+const PORT = 3333;
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT} 🚀`);
+});
