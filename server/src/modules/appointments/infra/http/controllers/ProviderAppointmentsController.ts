d9284a6248b0a32dba3a4647464e2dfd397a7865
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-09-15 17:38:21 -0300

    Ajustando a API: Serialização de cache e adicionando requisições do insomnia

--- server/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ server/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,3 +1,4 @@
+import { classToClass } from 'class-transformer';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
@@ -19,7 +20,7 @@ class ListProviderAppointmentsController {
       year: Number(year),
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
 
